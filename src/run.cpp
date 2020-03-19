ab40acac393241f2ef34435cc24b6108c1ca4189
Maxim <eremenko98@yandex.ru>
2020-03-19 23:31:03 +0700

    Rewrite do_s.cpp && add // in run.cpp

--- src/run.cpp
+++ src/run.cpp
@@ -5,12 +5,12 @@ extern string do_s(vector <action> &rules, string main_row, int i, int point);
 void RUN(vector<action>& rules, int end, string main_row)
 {
   {
-    string current_state = rules[0].next_state;
+    //string current_state = rules[0].next_state;
     for (int i = 0; i <= end; i++)
     {
           main_row = do_s(rules, main_row, i, point);
           print_string(main_row);
-          point = move(point, rules, i);
+          point = move(  point,rules, i);
     }
   }
-} 
+}
