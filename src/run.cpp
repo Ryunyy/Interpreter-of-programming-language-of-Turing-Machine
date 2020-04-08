bed44ab292de14c93483b982a8b6f406fb4b3063
Sviatoslav Sentyurov <khirigiri@gmail.com>
2020-04-08 17:04:46 +0700

    Fix error in run.cpp

--- src/run.cpp
+++ src/run.cpp
@@ -2,7 +2,7 @@
 extern void print_string(string row);
 extern int move(int point, vector<action>& rules, int i);
 extern string do_s(vector<action>& rules, string main_row, int point);
-void RUN(vector<action>& rules string main_row)
+void RUN(vector<action>& rules, string main_row)
 {
     int i = 0, point = 0;
     string current_state = rules[point].state;
