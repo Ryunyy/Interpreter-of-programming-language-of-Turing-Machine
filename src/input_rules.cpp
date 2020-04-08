381db608e01c2f6ba96483ec5f86e2795efeeaa4
Sviatoslav Sentyurov <khirigiri@gmail.com>
2020-04-08 16:46:06 +0700

    Fix error in input_rules.cpp

--- src/input_rules.cpp
+++ src/input_rules.cpp
@@ -4,20 +4,22 @@ extern void RUN(vector<action>& rules, string main_row);
 void input_rules(vector<action>& rules, string main_row)
 {
     int fl;
-    for (i = 0; 1; i++)
+    action element;
+    int fl, i;
+    for (i = 0; 1; i++) {
         rules.push_back(element);
-    cin >> rules[i].state;
-    cin >> rules[i].exp_symbol;
-    cin >> rules[i].new_symb;
-    cin >> rules[i].move;
-    cin >> rules[i].next_state;
-    fl = check_error(rules, i);
-    if (!fl) {
-        cout << "ERROR";
-        break;
+        cin >> rules[i].state;
+        cin >> rules[i].exp_symbol;
+        cin >> rules[i].new_symb;
+        cin >> rules[i].move;
+        cin >> rules[i].next_state;
+        fl = check_error(rules, i);
+        if (!fl) {
+            cout << "ERROR";
+            break;
+        }
+        if (rules[i].next_state == "##")
+            break;
     }
-    if (rules[i].next_state == "##")
-        break;
-}
-RUN(rules, main_row);
+    RUN(rules, main_row);
 }
